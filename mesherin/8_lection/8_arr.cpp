6b215bafd86288039ee36e308d06d5109ec789f9
foyreet <153296403+foyreet@users.noreply.github.com>
2023-12-26 16:43:33 +0300

    Update 8_arr.cpp

--- mesherin/8_lection/8_arr.cpp
+++ mesherin/8_lection/8_arr.cpp
@@ -1,8 +1,3 @@
 Массивы переменной длины в с++ запрещены(выдаёт warning)
-То есть 
-int n;
-STD::cin >> n;
-int a[n];
-for (int i = 0; I < n; I++) {
- STD::cin >> a[I];
-}
+
+https://pastebin.com/LvtZnYVj
