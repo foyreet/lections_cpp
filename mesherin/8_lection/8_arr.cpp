19e3a86a3fc1aa0132107fe9be44aa17ad33db57
foyreet <153296403+foyreet@users.noreply.github.com>
2023-12-26 16:44:33 +0300

    Update 8_arr.cpp

--- mesherin/8_lection/8_arr.cpp
+++ mesherin/8_lection/8_arr.cpp
@@ -1,3 +1,17 @@
 Массивы переменной длины в с++ запрещены(выдаёт warning)
 
-https://pastebin.com/LvtZnYVj
+#include <iostream>
+int main(int argc, char *argv[])
+{
+	int n;
+	std::cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> a[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << a[i];
+	}
+}
